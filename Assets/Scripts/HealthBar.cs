b8544f9adc908d308c639ce9fcf2af01c0d7f811
dspages <dspages@gmail.com>
2019-10-05 04:47:05 -0700

    Added health bars to characters in the combat scene

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            0,
+            transform.eulerAngles.z
+        );
+    }
+}
