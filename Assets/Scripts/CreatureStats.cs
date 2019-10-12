a6877661d2fb21f7bdd99eef58254196f41bbe61
dspages <dspages@gmail.com>
2019-10-12 10:55:04 -0700

    Adds bonus damage for rear attacks and critical hits

--- Assets/Scripts/CreatureStats.cs
+++ Assets/Scripts/CreatureStats.cs
@@ -52,37 +52,37 @@ public class CreatureStats : MonoBehaviour
     // Average of strength and strength times lifepercent
     protected int GetEffectiveStrength()
     {
-        float[] ar = new float[2] { Strength, Strength * percentHealth() };
+        float[] ar = new float[2] { Strength, Strength * PercentHealth() };
         return Mathf.RoundToInt(ar.Average());
     }
 
     // Average of agility and agility times lifepercent
     protected int GetEffectiveAgility()
     {
-        float[] ar = new float[2] { Agility, Agility * percentHealth() };
+        float[] ar = new float[2] { Agility, Agility * PercentHealth() };
         return Mathf.RoundToInt(ar.Average());
     }
 
     // Average of intelligence and intelligence times lifepercent
     protected int GetEffectiveIntelligence()
     {
-        float[] ar = new float[2] { Intelligence, Intelligence * percentHealth() };
+        float[] ar = new float[2] { Intelligence, Intelligence * PercentHealth() };
         return Mathf.RoundToInt(ar.Average());
     }
 
     // Average of Speed and Speed times lifepercent
     protected int GetEffectiveSpeed()
     {
-        float[] ar = new float[2] { Speed, Speed * percentHealth() };
+        float[] ar = new float[2] { Speed, Speed * PercentHealth() };
         return Mathf.RoundToInt(ar.Average());
     }
 
-    private float percentHealth()
+    private float PercentHealth()
     {
         return (float)CurrentHealth / (float)MaxHealth;
     }
 
-    private float percentStamina()
+    private float PercentStamina()
     {
         return (float)CurrentStamina / (float)MaxStamina;
     }
@@ -103,28 +103,35 @@ public class CreatureStats : MonoBehaviour
             CurrentHealth -= (amount - CurrentStamina);
             CurrentStamina = 0;
         }
-        healthBarScript.SetPercent(percentHealth());
-        staminaBarScript.SetPercent(percentStamina());
+        healthBarScript.SetPercent(PercentHealth());
+        staminaBarScript.SetPercent(PercentStamina());
     }
 
-    private int hitChance()
+    private int HitChance()
     {
         return 75 + GetEffectiveAgility() / 4;
     }
 
-    private int dodgeChance()
+    private int DodgeChance()
     {
         return 0 + GetEffectiveAgility() / 2;
     }
 
-    private bool percentRoll(int percent)
+    private bool PercentRoll(int percent)
     {
         return rng.Next(0, 99) < percent;
     }
 
-    private int damageInflicted()
+    // Damage from a basic attack is 1-11 plus half of strength.
+    private int DamageInflicted()
     {
-        return rng.Next(0, 10) + GetEffectiveStrength() / 2;
+        return rng.Next(1, 11) + GetEffectiveStrength() / 2;
+    }
+
+    // Bonus damage from a rear attack is 1-5 plus a quarter of agility.
+    private int BonusRearDamage()
+    {
+        return rng.Next(1, 5) + GetEffectiveAgility() / 4;
     }
 
     private void DisplayPopup(string text)
@@ -133,19 +140,70 @@ public class CreatureStats : MonoBehaviour
         textLabelText = text;
     }
 
+    // Returns true if floats are within 15.0f of each other.
+    // compare Mathf.Approximately.
+    private bool VeryApproximately(float f1, float f2)
+    {
+        if (Mathf.Abs(f1 - f2) < 15.0f) return true;
+        return false;
+    }
+
+    // Returns true if the target is being attacked from the side or rear.
+    // Side/read attacks double the chance of a critical hit.
+    public bool IsFlanking(CreatureStats target)
+    {
+        float rotation1 = target.transform.eulerAngles.y;
+        float rotation2 = transform.eulerAngles.y;
+        bool flanking = !VeryApproximately(Mathf.Abs(rotation1 - rotation2), 180.0f);
+        return flanking;
+    }
+
+    // Returns true if the target is being attacked from the rear.
+    // Rear attacks do a bonus 0-10 damage plus one half of agility.
+    public bool IsBehind(CreatureStats target)
+    {
+        float rotation1 = target.transform.eulerAngles.y;
+        float rotation2 = transform.eulerAngles.y;
+        bool behind = VeryApproximately(Mathf.Abs(rotation1 - rotation2), 0.0f) || VeryApproximately(Mathf.Abs(rotation1 - rotation2), 360.0f);
+        return behind;
+    }
+
+    public bool IsCrit(CreatureStats target)
+    {
+        int chance = GetEffectiveIntelligence() / 10;
+        if (IsFlanking(target))
+        {
+            chance *= 2;
+        }
+        return PercentRoll(chance);
+    }
+
     public void PerformAttack(CreatureStats target)
     {
-        if (!percentRoll(hitChance())) {
+        if (!PercentRoll(HitChance())) {
             DisplayPopup("Miss!");
             return;
         }
-        if (percentRoll(target.dodgeChance()))
+        if (PercentRoll(target.DodgeChance()))
         {
             target.DisplayPopup("Dodge!");
             return;
         }
-        int dam = damageInflicted();
-        target.DisplayPopup(dam + " damage inflicted!");
+        int dam = DamageInflicted();
+        if (IsBehind(target))
+        {
+            DisplayPopup("Rear attack!");
+            dam += BonusRearDamage();
+        }
+        if (IsCrit(target))
+        {
+            target.DisplayPopup("CRITICAL HIT! " + dam + " damage inflicted!");
+            dam += (dam / 2);
+        }
+        else
+        {
+            target.DisplayPopup(dam + " damage inflicted!");
+        }
         target.ReceiveDamage(dam);
     }
 }
