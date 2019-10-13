9670bf9bfc10f44ccf3b45d62da94dc1e54746fb
dspages <dspages@gmail.com>
2019-10-13 14:20:18 -0700

    Camera now can be scrolled by mousing to the edge of the screen

--- Assets/Scripts/CombatCamera.cs
+++ Assets/Scripts/CombatCamera.cs
@@ -16,6 +16,41 @@ public class CombatCamera : MonoBehaviour
         transform.Rotate(Vector3.up, -90, Space.Self);
     }
 
+    void MouseScroll()
+    {
+        Vector3 CameraSpeed = new Vector3(0f, 0f, 0f);
+        Vector3 CurrentMousePosition = Input.mousePosition;
+        if (CurrentMousePosition.x >= Screen.width - 20)
+        {
+            CameraSpeed.x = Time.deltaTime * 10f;
+        }
+        if (CurrentMousePosition.x <= 20)
+        {
+            CameraSpeed.x = Time.deltaTime * -10f;
+        }
+        if (CurrentMousePosition.y >= Screen.height - 20)
+        {
+            CameraSpeed.z = Time.deltaTime * 10f;
+        }
+        if (CurrentMousePosition.y <= 20)
+        {
+            CameraSpeed.z = Time.deltaTime * -10f;
+        }
+        transform.Translate(CameraSpeed, Space.World);
+
+        // Clamp the camera position so it doesn't go too far away from the grid.
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -5f, 5f),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z, -10f, 1f)
+        );
+    }
+
+    void Update()
+    {
+        MouseScroll();
+    }
+
     private IEnumerator GradualizeMove(Vector3 loc)
     {
         Vector3 initial = transform.position;
@@ -35,7 +70,7 @@ public class CombatCamera : MonoBehaviour
     public void ZoomNear(CombatController target)
     {
         Vector3 newTarget = target.transform.position;
-        newTarget.z -= 5.5f;
+        newTarget.z -= 3.5f;
         newTarget.y = transform.position.y;
         StartCoroutine(GradualizeMove(newTarget));
     }
