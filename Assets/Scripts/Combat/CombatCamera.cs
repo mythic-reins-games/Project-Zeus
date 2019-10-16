b1d46d3a35005367d75131e3bfebec96f1f147e9
dspages <dspages@gmail.com>
2019-10-15 22:23:50 -0700

    Fixes camera rotation functionality

--- Assets/Scripts/Combat/CombatCamera.cs
+++ Assets/Scripts/Combat/CombatCamera.cs
@@ -4,16 +4,51 @@ using System.Collections;
 public class CombatCamera : MonoBehaviour
 {
 
+    const int DIR_NORTH = 0;
+    const int DIR_EAST = 1;
+    const int DIR_SOUTH = 2;
+    const int DIR_WEST = 3;
+
+    float[,] boundsByDirection = new float[,] {
+        {0f, 4f, -4f, -9f},
+        {4f, 9f, 0f, -4f},
+        {9f, 4f, -4f, 0f},
+        {4f, 0f, -9f, -4f},
+    };
+
+    int dir = 0;
+
     private float updateTimeSeconds = 0.25f;
 
     public void RotateLeft()
     {
         transform.Rotate(Vector3.up, 90, Space.Self);
+        // Easier to reason about than dir - 1 because C# has slightly confusing handling of modulo of negative numbers.
+        dir = (dir + 3) % 4;
     }
 
     public void RotateRight()
     {
         transform.Rotate(Vector3.up, -90, Space.Self);
+        dir = (dir + 1) % 4;
+    }
+
+    void CameraSpeedCalc(ref Vector3 cameraSpeed, int edge)
+    {
+        switch ((dir + edge) % 4) {
+            case DIR_NORTH:
+                cameraSpeed.x = Time.deltaTime * 10f;
+                break;
+            case DIR_EAST:
+                cameraSpeed.z = Time.deltaTime * 10f;
+                break;
+            case DIR_SOUTH:
+                cameraSpeed.x = Time.deltaTime * -10f;
+                break;
+            case DIR_WEST:
+                cameraSpeed.z = Time.deltaTime * -10f;
+                break;
+        }
     }
 
     void MouseScroll()
@@ -22,32 +57,40 @@ public class CombatCamera : MonoBehaviour
         Vector3 currentMousePosition = Input.mousePosition;
         if (currentMousePosition.x >= Screen.width - 20)
         {
-            cameraSpeed.x = Time.deltaTime * 10f;
+            CameraSpeedCalc(ref cameraSpeed, DIR_NORTH);
         }
         if (currentMousePosition.x <= 20)
         {
-            cameraSpeed.x = Time.deltaTime * -10f;
+            CameraSpeedCalc(ref cameraSpeed, DIR_SOUTH);
         }
         if (currentMousePosition.y >= Screen.height - 20)
         {
-            cameraSpeed.z = Time.deltaTime * 10f;
+            CameraSpeedCalc(ref cameraSpeed, DIR_EAST);
         }
         if (currentMousePosition.y <= 20)
         {
-            cameraSpeed.z = Time.deltaTime * -10f;
+            CameraSpeedCalc(ref cameraSpeed, DIR_WEST);
         }
         transform.Translate(cameraSpeed, Space.World);
 
         // Clamp the camera position so it doesn't go too far away from the grid.
         transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, -4f, 4f),
+            Mathf.Clamp(transform.position.x, boundsByDirection[dir, 2], boundsByDirection[dir, 1]),
             transform.position.y,
-            Mathf.Clamp(transform.position.z, -9f, 0f)
+            Mathf.Clamp(transform.position.z, boundsByDirection[dir, 3], boundsByDirection[dir, 0])
         );
     }
 
     void Update()
     {
+        if (Input.GetKeyDown("q"))
+        {
+            RotateLeft();
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            RotateRight();
+        }
         MouseScroll();
     }
 
